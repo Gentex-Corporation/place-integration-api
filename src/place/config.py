d8e35b3f152e0711b305c9f65df1d8e256421977
andrewtran-gentex <andrew.tran@gentex.com>
2026-05-13 15:24:06 -0400

    fix fulfillment url (#7)
    * fix fulfillment url
    
    * bump version

--- src/place/config.py
+++ src/place/config.py
@@ -9,7 +9,7 @@ EXPIRE_SEC = decouple.config("AWS_EXPIRE_SEC", default=86400)
 KEEP_ALIVE_SEC = decouple.config("AWS_KEEP_ALIVE_SEC", default=30)
 FULFILLMENT_URL = decouple.config(
     "AWS_FULFILLMENT_URL",
-    default="https://ymxta4fyrl.execute-api.us-east-2.amazonaws.com/prod/fulfillment",
+    default="https://14kbj32umd.execute-api.us-east-1.amazonaws.com/prod/fulfillment",
 )
 # Note: must be the regional IoT endpoint for now. Currently the custom IoT endpoint/domain configuration uses ApplicationProtocol.SECURE_MQTT
 # a second domain configuration must be created for ApplicationProtocol.MQTT_WSS in order to use the custom IoT endpoint instead.
