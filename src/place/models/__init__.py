7cf57d2da45207485aaa838b5873b3e37878f64e
Tran, Andrew <andrew.tran@gentex.com>
2026-06-30 14:15:35 +0000

    add device shadow model with alarm status

--- src/place/models/__init__.py
+++ src/place/models/__init__.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 from .credentials import Credentials
 from .discover_device import DiscoverDevice
 from .mqtt_message import MqttMessage
+from .device_shadow import AlarmStatus, PlaceDeviceShadow
 
 __all__ = [
     "Credentials",
     "DiscoverDevice",
     "MqttMessage",
+    "AlarmStatus",
+    "PlaceDeviceShadow",
 ]
-
