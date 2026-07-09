4297cff5fcee62fdec405db339d6187124aa4ad9
Tran, Andrew <andrew.tran@gentex.com>
2026-07-09 18:36:55 +0000

    add jwt decoder

--- src/place/auth/__init__.py
+++ src/place/auth/__init__.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from .srp_auth import login, get_iot_credentials
+from .tokens import decode_sub
 
 __all__ = [
     "login",
     "get_iot_credentials",
+	"decode_sub",
 ]
-
