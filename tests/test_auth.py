ef52e82ed53a78a28e32defbccb5fdb1ea773fae
andrewtran-gentex <andrew.tran@gentex.com>
2026-07-09 14:52:42 -0400

    Fix unit tests (#14)
    * get unit tests to run
    
    * bump version to v0.2.2

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,24 +1,14 @@
 from __future__ import annotations
 
 from unittest.mock import MagicMock, patch
+from place.auth import get_iot_credentials
 
-from place_integration_api.auth import get_credentials_via_cognito
 
-
-@patch("place_integration_api.auth.srp_auth.boto3")
-@patch("place_integration_api.auth.srp_auth.get_tokens_via_srp")
-def test_get_credentials_via_cognito_success(
-    mock_get_tokens_via_srp: MagicMock, mock_boto3: MagicMock
-) -> None:
+@patch("place.auth.srp_auth.boto3")
+def test_get_iot_credentials_success(mock_boto3: MagicMock) -> None:
     identity_client = MagicMock()
     mock_boto3.client.return_value = identity_client
 
-    mock_get_tokens_via_srp.return_value = {
-        "AuthenticationResult": {
-            "IdToken": "id-token",
-            "AccessToken": "access-token",
-        }
-    }
     identity_client.get_id.return_value = {"IdentityId": "identity-123"}
     identity_client.get_credentials_for_identity.return_value = {
         "Credentials": {
@@ -28,13 +18,10 @@ def test_get_credentials_via_cognito_success(
         }
     }
 
-    creds = get_credentials_via_cognito(
-        user_pool_id="pool",
-        client_id="client",
-        identity_pool_id="identity-pool",
-        username="user",
-        password="pass",
-        region="us-east-2",
+
+    creds = get_iot_credentials(
+        id_token="id-token",
+        access_token="access-token",
     )
 
     assert creds.access_key_id == "AKIA..."
@@ -42,4 +29,3 @@ def test_get_credentials_via_cognito_success(
     assert creds.session_token == "session"
     assert creds.identity_id == "identity-123"
     assert creds.access_token == "access-token"
-
